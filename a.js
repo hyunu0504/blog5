5a97d30c4cf81e0d6f608bbdc54f88e30e18d993
hyunu0504 <105877042+hyunu0504@users.noreply.github.com>
2022-06-08 18:14:17 +0900

    Add files via upload

---
+++ a.js
@@ -0,0 +1,13 @@
+function addSnow(){
+    const snows = document.createElement('div') // div를 생성하는것을 축약해서 snows라는 변수에 넣기
+    snows.classList.add('snow') // snows에 snow라는 class로 선언
+    document.body.append(snows) // 그것을 body요소에 넣기
+    snows.style.marginLeft = randomSnow(); // snows의 marginleft값을 randomSnow()로 지정
+}
+function randomSnow(){
+    return Math.floor( Math.random() * window.innerWidth ) // Math.randomn으로 무작위 값이 window 창의 가로크기 안에서의 값만
+    //    Math.fllor로  그 값의 소수값은 받지않음
+}
+for( i = 0; i < 1000; i++ ){ 
+    addSnow(); //1000번이 실행되는 for문
+}
